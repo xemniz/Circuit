1a03ebea5f542a061fac989caa2e67a11699ab08
Nikolay Konorev <sglvzom@gmail.com>
2017-10-24 23:47:08 +0300

    support lib gridlayout

--- app/src/main/java/circuit/ru/xmn/circuit/model/grid/MidiScreenGridAdapter.kt
+++ app/src/main/java/circuit/ru/xmn/circuit/model/grid/MidiScreenGridAdapter.kt
@@ -1,17 +1,10 @@
 package circuit.ru.xmn.circuit.model.grid
 
-import android.graphics.Color
+import android.support.v7.widget.GridLayout
 import android.view.Gravity
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.GridLayout
-import android.widget.TableLayout
-import android.widget.TableRow
-import com.sdsmdg.harjot.crollerTest.Croller
 import it.beppi.knoblibrary.Knob
-import ru.xmn.common.extensions.dp
 import ru.xmn.common.extensions.px
-import ru.xmn.common.extensions.views
 
 class MidiScreenGridAdapter(val layout: GridLayout,
                             controllers: List<MidiGridController>,
@@ -29,10 +22,11 @@ class MidiScreenGridAdapter(val layout: GridLayout,
 
     private fun bindView(gridPositionInfo: GridPositionInfo, listener: (Int) -> Unit) {
         val knob = KnobFactory.create(layout, listener)
-        val params = GridLayout.LayoutParams(GridLayout.spec(gridPositionInfo.row - 1,1f), GridLayout.spec(gridPositionInfo.column - 1,1f))
+        val params = GridLayout.LayoutParams(GridLayout.spec(gridPositionInfo.row - 1, 1f), GridLayout.spec(gridPositionInfo.column - 1, 1f))
         params.setGravity(Gravity.FILL)
         params.setMargins(4.px, 4.px, 4.px, 4.px)
-        params.height = 64.px
+        params.height = 0
+        params.width = 0
         knob.layoutParams = params
 
         layout.addView(knob)
